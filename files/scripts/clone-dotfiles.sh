7fd39f453ada6da0fa56faed2f038356f035854a
Daniel Mauderer <daniel.mauderer@outlook.com>
2025-10-21 09:51:47 +0200

    update clone script

--- files/scripts/clone-dotfiles.sh
+++ files/scripts/clone-dotfiles.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-mkdir -p /usr/local/share
+mkdir -p /usr/share/hypr
 
-if [ -d /usr/local/share/dotfiles/.git ]; then
+if [ -d /usr/share/.git ]; then
   echo "[script] Updating existing dotfiles repo..."
-  git -C /usr/local/share/dotfiles pull --ff-only || true
+  git -C /usr/share/dotfiles pull --ff-only || true
 else
   echo "[script] Cloning dotfiles repo..."
-  rm -rf /usr/local/share/dotfiles || true
-  git clone --depth=1 https://github.com/DanielMauderer/MyLinux.git /usr/local/share/dotfiles
+  rm -rf /usr/share/hypr || true
+  git clone --depth=1 https://github.com/DanielMauderer/MyLinux.git /usr/share
 fi
