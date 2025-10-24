c72a6a830ac6e306703b677ee7b9cf6678841098
Daniel Mauderer <daniel.mauderer@outlook.com>
2025-10-24 10:43:44 +0200

    fix script

--- files/system/firstboot.sh
+++ files/system/firstboot.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 LOGFILE="/var/log/firstboot.log"
 notify-send "Running first boot setup..."
-send-not
+
 {
 echo "🚀 Running first boot setup..."
 
@@ -12,11 +12,8 @@ USER_HOME="/home/${SUDO_USER:-$(logname)}"
 # Clone dotfiles
 if [ ! -d "$USER_HOME/.dotfiles" ]; then
   sudo -u "${SUDO_USER:-$(logname)}" git clone https://github.com/DanielMauderer/MyLinux "$USER_HOME/.dotfiles"
-  sudo -u "${SUDO_USER:-$(logname)}" bash -c "cd ~/.dotfiles && ./install.sh || true"
+  sudo -u "${SUDO_USER:-$(logname)}" bash -c "cd ~/.dotfiles && ./setup.sh || true"
 fi
-
-# Create toolbox
-sudo -u "${SUDO_USER:-$(logname)}" toolbox create default || true
 
 # Disable the service after running
 systemctl disable firstboot.service
